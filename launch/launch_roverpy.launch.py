f20f7c19baef3a1c6bd9ac55753749d7f00f6d6c
spuzzell <107437291+spuzzell@users.noreply.github.com>
2025-07-15 15:57:02 +0100

    Add files via upload

--- launch/launch_roverpy.launch.py
+++ launch/launch_roverpy.launch.py
@@ -82,48 +82,11 @@ def generate_launch_description():
 					('/cmd_vel_out','/diff_cont/cmd_vel')]
 	)
 	
-	robot_description = ParameterValue(
-        Command(['ros2 param get --hide-type /robot_state_publisher robot_description']),
-        value_type=str
-    )
-
-	controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
-
-	controller_manager = Node(
-		package="controller_manager",
-		executable="ros2_control_node",
-		parameters=[{'robot_description': robot_description},
-                    controller_params_file]
-	)
-
-	delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
-
-
-	diff_drive_spawner = Node(
-		package="controller_manager",
-		executable="spawner",
-		arguments=["diff_cont"],
-	)
-
-	delayed_diff_drive_spawner = RegisterEventHandler(
-        event_handler=OnProcessStart(
-            target_action=controller_manager,
-            on_start=[diff_drive_spawner],
-        )
-    )
-
-	joint_broad_spawner = Node(
-		package="controller_manager",
-		executable="spawner",
-		arguments=["joint_broad"],
-	)
-
-	delayed_joint_broad_spawner = RegisterEventHandler(
-        event_handler=OnProcessStart(
-            target_action=controller_manager,
-            on_start=[joint_broad_spawner],
-        )
-    )
+	controller = IncludeLaunchDescription(
+		PythonLaunchDescriptionSource([os.path.join(
+			get_package_share_directory('rover_controller'),'launch','controller.launch.py'
+		)]))
+	
 
 	# Combine and return all launch elements
 	return LaunchDescription([
@@ -132,7 +95,5 @@ def generate_launch_description():
         joystick,
         twist_mux,
         twist_stamper,
-		delayed_controller_manager,
-        delayed_diff_drive_spawner,
-        delayed_joint_broad_spawner
+		controller
     ])
